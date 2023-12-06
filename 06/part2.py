a1fcd407ed617af29c11dd9113282620c50263cb
Mihal Matej <Matej.Mihal@airbank.cz>
2023-12-06 11:15:43 +0100

    add day 6 solution bruteforce is the way to go

---
+++ 06/part2.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+with Path("./input.txt").open("r") as inp:
+    result = 0
+    seeds = []
+    maps = dict()
+    f, t = "", ""
+    map_num = 0
+    times, records = None, None
+    for line in inp:
+        key, nums = line.strip().split(":")
+        if key.strip() == "Time":
+            times = [int("".join([n for n in nums.strip().split() if n]))]
+        if key.strip() == "Distance":
+            records = [int("".join([n for n in nums.strip().split() if n]))]
+    result = 1
+    for limit, current_max in zip(times, records):
+        better = []
+        for candidate in range(0, limit):
+            actual = candidate * (limit - candidate)
+            if actual > current_max:
+                better.append(actual)
+        result *= len(better)
+    print(result)
