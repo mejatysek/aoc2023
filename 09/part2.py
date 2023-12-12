0bd66e1efab3e838cb0f49dde54c49b7cf93d77d
Mihal Matej <Matej.Mihal@airbank.cz>
2023-12-12 20:46:40 +0100

    add day 9 solution not sure why part2 work but who cares

---
+++ 09/part2.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+with Path("./input.txt").open("r") as inp:
+    result = 0
+    for line in inp:
+        nums = [int(x) for x in line.strip().split()]
+        rows = [list(reversed(nums))]
+
+        while not all([x == 0 for x in rows[-1]]):
+            last_row = rows[-1]
+            new_row = [b - a for a, b in zip(last_row, last_row[1:])]
+            rows.append(new_row)
+        rows[-1].append(0)
+
+        for i in range(2, len(rows)+1):
+            rows[-i].append(rows[-i+1][-1] + rows[-i][-1])
+        result += rows[0][-1]
+    print(result)
